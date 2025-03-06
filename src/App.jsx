4a111f111e739eb938f542850bd340b95a2d5a0b
BMukhammadaziz <MukhammadazizBakhromov@gmail.com>
2025-03-06 14:16:01 +0500

    Restaran

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Restorans from './Restorans/Restorans'
+
+function App() {
+
+  return (
+    <>
+        <Restorans />
+    </>
+  )
+}
+
+export default App
---
+++ src/Restorans/Restorans.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Restorans.css"
+
+const Restorans = () => {
+    return(
+        <>
+        
+        <div className="container">
+            <h1>
+                Restoran va kafelar
+            </h1>
+            <div className="boxs">
+                <div className="textBox">
+                    <h3>
+                        Bek Osiyo Yakkasaroy
+                    </h3>
+                    <span>
+                        0.22 km
+                    </span>
+                </div>
+                <div className="iconBox">
+                    <i class='bx bx-current-location'></i>
+                    <i class='bx bx-chevron-right'></i>
+                </div>
+            </div>
+            <div className="boxs">
+                <div className="textBox">
+                    <h3>
+                        Bek Osiyo Yakkasaroy
+                    </h3>
+                    <span>
+                        0.22 km
+                    </span>
+                </div>
+                <div className="iconBox">
+                    <i class='bx bx-current-location'></i>
+                    <i class='bx bx-chevron-right'></i>
+                </div>
+            </div>
+            <div className="boxs">
+                <div className="textBox">
+                    <h3>
+                        Bek Osiyo Yakkasaroy
+                    </h3>
+                    <span>
+                        0.22 km
+                    </span>
+                </div>
+                <div className="iconBox">
+                    <i class='bx bx-current-location'></i>
+                    <i class='bx bx-chevron-right'></i>
+                </div>
+            </div>
+            <div className="boxs">
+                <div className="textBox">
+                    <h3>
+                        Bek Osiyo Yakkasaroy
+                    </h3>
+                    <span>
+                        0.22 km
+                    </span>
+                </div>
+                <div className="iconBox">
+                    <i class='bx bx-current-location'></i>
+                    <i class='bx bx-chevron-right'></i>
+                </div>
+            </div>
+            <div className="boxs">
+                <div className="textBox">
+                    <h3>
+                        Bek Osiyo Yakkasaroy
+                    </h3>
+                    <span>
+                        0.22 km
+                    </span>
+                </div>
+                <div className="iconBox">
+                    <i class='bx bx-current-location'></i>
+                    <i class='bx bx-chevron-right'></i>
+                </div>
+            </div>
+        </div>
+
+        </>
+    )
+}
+export default Restorans;
