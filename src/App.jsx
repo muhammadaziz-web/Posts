e6aad624f0f49ceea9e1c26dcbee7e5bec0aad19
BMukhammadaziz <MukhammadazizBakhromov@gmail.com>
2025-03-06 20:49:23 +0500

    New data

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,33 @@
 import './App.css'
-import MapCards from './MapCards/MapCards'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Anjir from "./Maps/Anjir";
-import Anor from "./Maps/Anor";
-import Bek from "./Maps/Bek";
-import Besh from "./Maps/Besh";
-import Bosphorus from "./Maps/Bosphorus";
-import Chor from "./Maps/Chor";
-import Donerci from "./Maps/Donerci";
-import MarMar from "./Maps/MarMar";
-import Qanotchi from "./Maps/Qanotchi";
-import SimSim from "./Maps/SimSim";
+import placesData from "./data.json";
+import { lazy, Suspense } from "react";
+import PlaceCards from './Components/PlaceCards/PlaceCards';
 
 function App() {
-
   return (
     <>
-
       <Router>
         <Routes>
-        <Route path="/" element={<MapCards />} />
-        <Route path="/maps/anjir-rossiya" element={<Anjir />} />
-        <Route path="/maps/anor-rossiya" element={<Anor />} />
-        <Route path="/maps/bek-osiyo-yakkasaroy" element={<Bek />} />
-        <Route path="/maps/besh-qozon-glinka" element={<Besh />} />
-        <Route path="/maps/bosphorus-viaport" element={<Bosphorus />} />
-        <Route path="/maps/chor-minor" element={<Chor />} />
-        <Route path="/maps/donerci-hamdi-usta-glinka" element={<Donerci />} />
-        <Route path="/maps/mar-mar-rossiya" element={<MarMar />} />
-        <Route path="/maps/qanotchi-qushbegi" element={<Qanotchi />} />
-        <Route path="/maps/sim-sim-muqimiy" element={<SimSim />} />
+          <Route path="/" element={<PlaceCards />} />
+          {placesData.map((place) => {
+            const Component = lazy(() => import(`./Components/Place/${place.component}.jsx`));
+            return (
+              <Route
+                key={place.id}
+                path={place.path}
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Component />
+                  </Suspense>
+                }
+              />
+            );
+          })}
         </Routes>
       </Router>
-      
     </>
-  )
+  );
 }
+
 export default App;
